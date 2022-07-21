283e9af2241452cff24008f477a26c5165f8902a
nantipak tathong <auto0032@icloud.com>
2022-07-21 22:29:16 +0700

    5.4 TOT

---
+++ lab3/src/components/EventCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div vlass="event-card">
+    <span>@{{ event.time }}on{{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+import { objectExpression } from '@babel/types'
+
+export default {
+  name: 'EventCard',
+  props: {
+    event: {
+      type: objectExpression,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
